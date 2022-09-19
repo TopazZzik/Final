f79ed9db6be4d33aea918fa2cd70d35239623dff
Luda <millya92@gmail.com>
2022-09-19 19:06:29 +0300

    secarr

---
+++ mark/Program.cs
@@ -0,0 +1,20 @@
+﻿// написать программу которая из имеющегося массива строк формирует массив из строк,
+// длина которых меньше либо равна 3 символа.
+
+string [] array = {"let", "me", "pass", "thistask"};
+
+void secondArray (string [] array)
+{
+    string[] secondarr = new string [array.Length];
+    for (int i = 0; i<array.Length; i++)
+    {
+        if(array[i].Length<=3)
+        {
+            secondarr[i]=array[i];
+            Console.WriteLine(secondarr[i]);
+        }
+    }
+    Console.WriteLine();
+}
+
+
