b9c9bba72a9fe3f619fb4ede3f1bf9c166b0de7b
Luda <millya92@gmail.com>
2022-09-19 22:09:29 +0300

    finale

--- mark/Program.cs
+++ mark/Program.cs
@@ -1,28 +1,28 @@
 ﻿// написать программу которая из имеющегося массива строк формирует массив из строк,
 // длина которых меньше либо равна 3 символа.
 
-string [] array = {"let", "me", "pass", "thistask"};
+string[] array = { "let", "me", "pass", "thistask" };
 PrintArray(array);
 SecondArray(array);
 
 
 void PrintArray(string[] Array)
 {
-    for(int i = 0; i<array.Length; i++)
+    for (int i = 0; i < array.Length; i++)
     {
         Console.WriteLine(Array[i]);
     }
     Console.WriteLine();
 }
 
-void SecondArray (string [] array)
+void SecondArray(string[] array)
 {
-    string[] secondarr = new string [array.Length];
-    for (int i = 0; i<array.Length; i++)
+    string[] secondarr = new string[array.Length];
+    for (int i = 0; i < array.Length; i++)
     {
-        if(array[i].Length<=4)
+        if (array[i].Length <= 4)
         {
-            secondarr[i]=array[i];
+            secondarr[i] = array[i];
             Console.WriteLine(secondarr[i]);
         }
     }
