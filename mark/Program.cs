14f0f875e4bc131b66a9cacd4e85aa3208bdfe69
Luda <millya92@gmail.com>
2022-09-19 20:27:53 +0300

    arr

--- mark/Program.cs
+++ mark/Program.cs
@@ -3,7 +3,7 @@
 
 string [] array = {"let", "me", "pass", "thistask"};
 
-void secondArray (string [] array)
+void SecondArray (string [] array)
 {
     string[] secondarr = new string [array.Length];
     for (int i = 0; i<array.Length; i++)
@@ -13,6 +13,15 @@ void secondArray (string [] array)
             secondarr[i]=array[i];
             Console.WriteLine(secondarr[i]);
         }
+    }
+    Console.WriteLine();
+}
+
+void PrintArray(string[] Array)
+{
+    for(int i = 0; i<array.Length; i++)
+    {
+        Console.WriteLine(Array[i]);
     }
     Console.WriteLine();
 }
